7fc2ad3fb57378a3029aec2f1a6f907c7090d83e
fortunatefolly <williamharry.fortuna@journalism.cuny.edu>
2016-02-28 20:43:51 -0500

    Finished

--- site.js
+++ site.js
@@ -1,28 +1,76 @@
 
-function addition(){
-	var x = document.getElementById("numberOne").value;
-	var y = document.getElementById("numberTwo").value;
-	var answer = Number(x) + Number(y);
-	alert(answer);
-}
+//add
+document.getElementById("addition").onclick = function add(num1, num2){
+  //get first number for the variable
+  var num1 = document.getElementById('first').value;
+  //get the second number
+  var num2 = document.getElementById('second').value;
+  //equation
+  var r = parseFloat(num1) + parseFloat(num2);
+  //create the string
+  var adding = num1 + '+' + num2 + '=' + r;
+  // if then statement for empty input
+  if (num1 == "" || num2 == ""){
+  	document.getElementById('result').innerHTML = "Please enter Value"
+  } else {
+  	//hopefully will return string
+  	document.getElementById('result').innerHTML = adding;
+  	}
+  }
 
-function multiplication(){
-	var x = document.getElementById("numberOne").value;
-	var y = document.getElementById("numberTwo").value;
-	var answer = Number(x) * Number(y);
-	alert(answer);
-}
+// substract
+document.getElementById("subtract").onclick = function minus(num1, num2){
+  //get first number for the variable
+  var num1 = document.getElementById('first').value;
+  //get the second number
+  var num2 = document.getElementById('second').value;
+  //equation
+  var r = parseFloat(num1) - parseFloat(num2);
+  //create the string
+  var subtracting = num1 + '-' + num2 + '=' + r;
+  // if then statement for empty input
+  if (num1 == "" || num2 == ""){
+  	document.getElementById('result').innerHTML = "Please enter Value"
+  } else {
+  	//hopefully will return string
+  	document.getElementById('result').innerHTML = subtracting;
+  	}
+  }
+// multiply
+document.getElementById("multiply").onclick = function times(num1, num2){
+  //get first number for the variable
+  var num1 = document.getElementById('first').value;
+  //get the second number
+  var num2 = document.getElementById('second').value;
+  //equation
+  var r = parseFloat(num1) * parseFloat(num2);
+  //create the string
+  var multiplying = num1 + '*' + num2 + '=' + r;
+  // if then statement for empty input
+  if (num1 == "" || num2 == ""){
+  	document.getElementById('result').innerHTML = "Please enter Value"
+  } else {
+  	//hopefully will return string
+  	document.getElementById('result').innerHTML = multiplying;
+  	}
+  }
 
-function subtraction(){
-	var x = document.getElementById("numberOne").value;
-	var y = document.getElementById("numberTwo").value;
-	var answer = Number(x) - Number(y);
-	alert(answer);
-}
+// divide
+document.getElementById("divide").onclick = function division(num1, num2){
+  //get first number for the variable
+  var num1 = document.getElementById('first').value;
+  //get the second number
+  var num2 = document.getElementById('second').value;
+  //equation
+  var r = parseFloat(num1) / parseFloat(num2);
+  //create the string
+  var dividing = num1 + '/' + num2 + '=' + r;
+  // if then statement for empty input
+  if (num1 == "" || num2 == ""){
+  	document.getElementById('result').innerHTML = "Please enter Value"
+  } else {
+  	//hopefully will return string
+  	document.getElementById('result').innerHTML = dividing;
+  	}
+  }
 
-function division(){
-	var x = document.getElementById("numberOne").value;
-	var y = document.getElementById("numberTwo").value;
-	var answer = Number(x) / Number(y);
-	alert(answer);
-}
