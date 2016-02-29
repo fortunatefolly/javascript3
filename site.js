bdf69cd63ff82254b452326b418938374e6fc733
fortunatefolly <williamharry.fortuna@journalism.cuny.edu>
2016-02-28 19:10:28 -0500

    First Commit

---
+++ site.js
@@ -0,0 +1,28 @@
+
+function addition(){
+	var x = document.getElementById("numberOne").value;
+	var y = document.getElementById("numberTwo").value;
+	var answer = Number(x) + Number(y);
+	alert(answer);
+}
+
+function multiplication(){
+	var x = document.getElementById("numberOne").value;
+	var y = document.getElementById("numberTwo").value;
+	var answer = Number(x) * Number(y);
+	alert(answer);
+}
+
+function subtraction(){
+	var x = document.getElementById("numberOne").value;
+	var y = document.getElementById("numberTwo").value;
+	var answer = Number(x) - Number(y);
+	alert(answer);
+}
+
+function division(){
+	var x = document.getElementById("numberOne").value;
+	var y = document.getElementById("numberTwo").value;
+	var answer = Number(x) / Number(y);
+	alert(answer);
+}
